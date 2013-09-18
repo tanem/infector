818f1250f4d47d713bb28619a0b6543671d63b27
tanem <tane.morgan@gmail.com>
2013-09-18 16:03:47 +1200

    Swapped lodash for underscore.

--- test/helpers/globals.js
+++ test/helpers/globals.js
@@ -3,7 +3,6 @@
 var path = require('path');
 
 global.expect = require('expect.js');
-global.sinon = require('sinon');
 global.lib = function(filePath){
   return require(path.join(__dirname, '../../lib', filePath));
 };
--- test/spec/infector_spec.js
+++ test/spec/infector_spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _ = require('lodash'),
+var _ = require('underscore'),
   Infector = lib('infector');
 
 describe('infector', function(){
